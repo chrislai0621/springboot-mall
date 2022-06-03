68a1862c2e56199db8b3b0b7e8b99454b9af59d5
ChrisLai <chris.lai234621@gmail.com>
2022-06-03 11:49:57 +0800

    帳號功能-單元測試

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS product
 CREATE TABLE user
 (
     user_id            INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    email              VARCHAR(256) NOT NULL UNIQUE KEY,
+    email              VARCHAR(256) NOT NULL UNIQUE,
     password           VARCHAR(256) NOT NULL,
     created_date       TIMESTAMP    NOT NULL,
     last_modified_date TIMESTAMP    NOT NULL
